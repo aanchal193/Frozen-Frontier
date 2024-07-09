b81c137377f21fb98155612f3fd9eef4404bd61d
Aanchal Sharma <118661203+aanchal193@users.noreply.github.com>
2024-07-09 17:48:29 +0530

    Add files via upload

---
+++ random_agent.py
@@ -0,0 +1,46 @@
+import gym
+
+
+def random_agent(env, observation):
+    return env.action_space.sample()
+
+
+def training(env, num_episodes):
+    total_reward = 0
+
+    for _ in range(num_episodes):
+        observation = env.reset()
+        done = False
+        episode_reward = 0
+        while not done:
+            action = random_agent(env, observation)
+            observation, reward, done, _, _ = env.step(action)
+            episode_reward += reward
+        total_reward += episode_reward
+
+    print(f"Recompensa média: {total_reward / num_episodes}")
+
+
+def demo_agent(env, num_episodes=1):
+    for episode in range(num_episodes):
+        observation, _ = env.reset()
+        done = False
+        print("\nEpisode:", episode + 1)
+        while not done:
+            env.render()
+            action = random_agent(env, observation)
+            observation, _, done, _, _ = env.step(action)
+        env.render()
+
+
+def main():
+    env = gym.make("FrozenLake-v1")
+    num_episodes = 10000
+
+    training(env, num_episodes)
+
+    visual_env = gym.make("FrozenLake-v1", render_mode='human')
+    demo_agent(visual_env, 3)
+
+if __name__ == '__main__':
+    main()
